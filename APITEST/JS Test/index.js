69a7e012ef338daa02cbf641b8057b3b9001c288
CarlosAstengo <a01570405@itesm.mx>
2022-03-23 21:10:15 -0600

    terminado?

--- APITEST/JS Test/index.js
+++ APITEST/JS Test/index.js
@@ -1,112 +1,157 @@
-//http://numbersapi.com/#42
-
 const axios = require('axios').default;
-const api = "http://numbersapi.com/";
-const math = "/math";
-const trivia = "";
-const date = "/date";
-const randomTrivia = "random/trivia";
-const randomYear = "random/year";
-const randomDate = "random/date";
-const randomMath= "random/math";
-const birthday = "06/30";
-const favoriteNumber = "69";
-var today = new Date();
-const min = 1;
-const max = 365;
-const seed = parseInt((today.getMonth()).toString() +(today.getDay()).toString());
-var key = ((seed * 9301 + 49297) % 233280)/233280;
-key = Math.floor(min + key * (max - min));
 
-const api2 = "https://ohmanda.com/api/horoscope/";
-const sign = "cancer";
+const horoscopeApi = "https://ohmanda.com/api/horoscope/";
+const numbersApi = "http://numbersapi.com/";
+const mathPath = "/math";
+const datePath = "/date";
+const randomTriviaPath = "random/trivia";
+const randomYearPath = "random/year";
+const randomDatePath = "random/date";
+const randomMathPath= "random/math";
 
-//get birthday trivia message
-axios.get(api + birthday + date)
-    .then((data) => {
-        console.log("1: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+var currentDate = new Date();
 
-//get favoriteNumber trivia message
-axios.get(api + favoriteNumber + trivia)
-    .then((data) => {
-        console.log("2: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+var name = "";
+var birthday = 0;
+var favoriteNumber = 0;
 
-//get favoriteNumber math message
-axios.get(api + favoriteNumber + math)
-    .then((data) => {
-        console.log("3: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+function simulateGetData() {
+    name = "Carlos Astengo";
+    birthday = 973296000 * 1000; // convertir a milisegundos
+    birthday = new Date(birthday);
+    favoriteNumber = 25;
+    favoriteNumber = favoriteNumber.toString();
+}
 
-//get daily trivia fact
-axios.get(api + key.toString() + math)
-    .then((data) => {
-        console.log("4: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+function getPersonalTrivia() {
+    //convert birthday to api format
+    var birthdayPath = (birthday.getMonth()+1).toString() + "/" +(birthday.getDate()).toString()
 
-//get today trivia date fact
-axios.get(api + ((today.getMonth()+1).toString() + '/' +today.getDate().toString()) + date)
-    .then((data) => {
-        console.log("5: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    }) 
+    //calculate zodiac sign
+    const x = parseInt((birthday.getMonth()+1).toString() + (("0" + birthday.getDate()).slice(-2)));
+    const sign = x >= 121 && x <= 218 ? 'Aquarius' : x >= 219 && x <= 320 ? 'Pisces' : x >= 321 && x <= 420 ? 'Aries' : x >= 421 && x <= 521 ? 'Taurus' : x >= 522 && x <= 621 ? 'Gemini' : x >= 622 && x <= 722 ? 'Cancer' : x >= 723 && x <= 823 ? 'Leo' : x >= 824 && x <= 923 ? 'Virgo' : x >= 924 && x <= 1023 ? 'Libra' : x >= 1024 && x <= 1122 ? 'Scorpio' : x >= 1123 && x <= 1221 ? 'Sagittarius' : x >= 1222 || x <= 120 ? 'Capricorn' : void 0;
 
-//get random trivia
-axios.get(api + randomTrivia)
-    .then((data) => {
-        console.log("6: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+    //get birthday trivia message
+    axios.get(numbersApi + birthdayPath + datePath)
+        .then((response) => {
+            console.log("Did you know that your birthday on ")
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error al conseguir trivia del cumpleaños \n");
+        })
 
-//get random year
-axios.get(api + randomYear)
-    .then((data) => {
-        console.log("7: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+    //get favoriteNumber trivia message
+    axios.get(numbersApi + favoriteNumber)
+        .then((response) => {
+            console.log("Did yo know that your favorite number ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error al conseguir trivia del numero favorito \n");
+        })
 
-//get random date
-axios.get(api + randomDate)
-    .then((data) => {
-        console.log("8: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+    //get favoriteNumber math message
+    axios.get(numbersApi + favoriteNumber + mathPath)
+        .then((response) => {
+            console.log("Did yo know that your favorite number ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error al conseguir trivia de mate del numero favorito \n");
+        })
 
-//get random math
-axios.get(api + randomMath)
-    .then((data) => {
-        console.log("9: " + data.data + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+    //get horoscope
+    axios.get(horoscopeApi + sign.toLowerCase())
+        .then((response) => {
+            //Posible bug donde el dia del cumpleaños es uno menos del normal
+            console.log("For you " + sign + ": ");
+            console.log(response.data.horoscope + "\n");
+        })
+        .catch((error) => {
+            console.log(sign);
+            console.log("error al conseguir horoscopo \n");
+        })
+}
 
-//get horoscope
-axios.get(api2 + sign)
-    .then((data) => {
-        console.log("10: " + data.data.horoscope + "\n");
-    })
-    .catch((error) => {
-        console.log("error en cumpleaños \n");
-    })
+function getRandomTrivia(){
+    //get random trivia
+    axios.get(numbersApi + randomTriviaPath)
+        .then((response) => {
+            console.log("Did you know that the number ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error en año trivia \n");
+        })
+
+    //get random year
+    axios.get(numbersApi + randomYearPath)
+        .then((response) => {
+            console.log("Did you know that the year ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error en año random \n");
+        })
+
+    //get random date
+    axios.get(numbersApi + randomDatePath)
+        .then((response) => {
+            console.log("Did you know that ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error en dia random \n");
+        })
+
+    //get random math
+    axios.get(numbersApi + randomMathPath)
+        .then((response) => {
+            console.log("Did you know that the number ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error en mate random \n");
+        })
+}
+
+function getDailyTrivia(){
+
+    const min = 1;
+    const max = 365;
+    const seed = parseInt((currentDate.getMonth()).toString() +(currentDate.getDate()).toString());
+    var key = ((seed * 9301 + 49297) % 233280)/233280;
+    key = Math.floor(min + key * (max - min));
+
+    //get daily trivia fact
+    axios.get(numbersApi + key.toString() + mathPath)
+        .then((response) => {
+            console.log("Today's number is " + key + ", ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error al conseguir numero del dia \n");
+        })
+
+    //get today trivia date fact
+    axios.get(numbersApi + ((currentDate.getMonth()+1).toString() + '/' +currentDate.getDate().toString()) + datePath)
+        .then((response) => {
+            console.log("Did you know that today, ");
+            console.log(response.data + "\n");
+        })
+        .catch((error) => {
+            console.log("error al conseguir la trivia de hoy \n");
+        }) 
+}
+
+simulateGetData();
+//getPersonalTrivia();
+//getRandomTrivia();
+getDailyTrivia();
+
+
+
+
+
+
